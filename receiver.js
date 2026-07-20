080d892b526531dcf850b3a17f6d6653094fe071
Hennadii Ponomarov <ponogeen@MacBook-Air-Hennadii.local>
2026-07-20 12:48:42 +0200

    Enforce selected HLS quality in receiver

--- receiver.js
+++ receiver.js
@@ -1,6 +1,26 @@
 const context = cast.framework.CastReceiverContext.getInstance();
 const playerManager = context.getPlayerManager();
 
+function limitMasterPlaylist(manifest, maxHeight) {
+  const lines = manifest.split(/\r?\n/);
+  const filtered = [];
+
+  for (let index = 0; index < lines.length; index += 1) {
+    const line = lines[index];
+    const match = /^#EXT-X-STREAM-INF:.*RESOLUTION=\d+x(\d+)/.exec(line);
+    if (match && Number(match[1]) > maxHeight) {
+      // In an HLS master playlist the URI is the next line after STREAM-INF.
+      if (index + 1 < lines.length && !lines[index + 1].startsWith('#')) {
+        index += 1;
+      }
+      continue;
+    }
+    filtered.push(line);
+  }
+
+  return filtered.join('\n');
+}
+
 playerManager.setMediaPlaybackInfoHandler((loadRequest, playbackConfig) => {
   const drm = loadRequest.media?.customData || loadRequest.customData || {};
 
@@ -23,6 +43,8 @@ playerManager.setMediaPlaybackInfoHandler((loadRequest, playbackConfig) => {
         maxHeight: drm.maxHeight,
       },
     };
+    playbackConfig.manifestHandler = (manifest, responseInfo, shakaRequest) =>
+      limitMasterPlaylist(manifest, drm.maxHeight);
   }
 
   return playbackConfig;
